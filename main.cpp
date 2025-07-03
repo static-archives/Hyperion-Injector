fd51eba45a93c0a778f5996b7071f9b20393a854
static-archives <statically@proton.me>
2025-07-03 03:08:14 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 typedef int(__fastcall* t_inject)(const DWORD pid, const char* const dll_path);
 typedef bool(__fastcall* t_update)();
 
-std::string get_current_directory()
+static std::string get_current_directory()
 {
 	CHAR exe_path[MAX_PATH] = { 0 };
 	GetModuleFileNameA(NULL, exe_path, MAX_PATH);
@@ -19,23 +19,36 @@ std::string get_current_directory()
 
 int main()
 {
+	// Find the game window if its open, or wait
 	HWND hwnd = FindWindowA(NULL, "Roblox");
-	if (hwnd != INVALID_HANDLE_VALUE)
+	while (!hwnd)
 	{
+		system("cls");
+		std::cout << "Waiting for game..." << std::endl;
+		Sleep(100);
+		hwnd = FindWindowA(NULL, "Roblox");
+	}
+	
+	if (hwnd)
+	{
+		// Get the process id from the window 
 		DWORD pid;
 		GetWindowThreadProcessId(hwnd, &pid);
 
 		std::string current_dir = get_current_directory();
 
+		// Load the injector api
 		HMODULE inject_module = LoadLibraryA(std::string(current_dir + "hyperion_inject.dll").c_str());
 		if (!inject_module)
 			return 0;
 
 		std::cout << "Injecting... Module: " << std::hex << inject_module << std::endl;
 
+		// Locate the two api functionz
 		t_inject f_inject = reinterpret_cast<t_inject>(GetProcAddress(inject_module, "inject"));
 		t_update f_update = reinterpret_cast<t_update>(GetProcAddress(inject_module, "update"));
 
+		// Inject the dll into the process id 
 		if (f_inject(pid, std::string(current_dir + "hello_world.dll").c_str()) != 0)
 		{
 			while (f_update());
