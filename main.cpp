43e8eb393594a21e5f255f748b118c2ade189b23
static-archives <statically@proton.me>
2025-07-04 15:28:42 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <Windows.h>
+#include <vector>
 #include <iostream>
+#include <tlhelp32.h>
 
 // Declarations for hyperion_inject.dll imports
-typedef int(__fastcall* t_inject)(const DWORD pid, const char* const dll_path);
+typedef int(__fastcall* t_inject)(const DWORD process_id, const DWORD thread_id, const char* const dll_path);
 typedef bool(__fastcall* t_update)();
 
-static std::string get_current_directory()
+std::string get_current_directory()
 {
 	CHAR exe_path[MAX_PATH] = { 0 };
 	GetModuleFileNameA(NULL, exe_path, MAX_PATH);
@@ -17,39 +19,81 @@ static std::string get_current_directory()
 	return current_dir;
 }
 
+static std::vector<DWORD> enumerate_threads(const DWORD pid)
+{
+	std::vector<DWORD> tids = {};
+
+	HANDLE h_snap_thread = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+	if (h_snap_thread != INVALID_HANDLE_VALUE)
+	{
+		THREADENTRY32 te{ 0 };
+		te.dwSize = sizeof(te);
+
+		if (Thread32First(h_snap_thread, &te))
+		{
+			do {
+				if (te.dwSize >= FIELD_OFFSET(THREADENTRY32, th32OwnerProcessID) +
+					sizeof(te.th32OwnerProcessID))
+				{
+					if (te.th32OwnerProcessID == pid)
+						tids.push_back(te.th32ThreadID);
+				}
+
+				te.dwSize = sizeof(te);
+			} while (Thread32Next(h_snap_thread, &te));
+		}
+		CloseHandle(h_snap_thread);
+	}
+
+	return tids;
+}
+
+const DWORD get_target_thread_id(const DWORD pid, const wchar_t* name)
+{
+	for (const auto tid : enumerate_threads(pid))
+	{
+		HANDLE h_thread = OpenThread(THREAD_ALL_ACCESS, false, tid);
+		if (h_thread == INVALID_HANDLE_VALUE)
+			continue;
+
+		PWSTR data;
+		HRESULT hr = GetThreadDescription(h_thread, &data);
+		if (SUCCEEDED(hr))
+		{
+			if (lstrcmpW(data, name) == 0)
+			{
+				LocalFree(data);
+				return tid;
+			}
+
+			LocalFree(data);
+		}
+	}
+
+	return 0;
+}
+
 int main()
 {
-	// Find the game window if its open, or wait
 	HWND hwnd = FindWindowA(NULL, "Roblox");
-	while (!hwnd)
+	if (hwnd != INVALID_HANDLE_VALUE)
 	{
-		system("cls");
-		std::cout << "Waiting for game..." << std::endl;
-		Sleep(100);
-		hwnd = FindWindowA(NULL, "Roblox");
-	}
-	
-	if (hwnd)
-	{
-		// Get the process id from the window 
 		DWORD pid;
 		GetWindowThreadProcessId(hwnd, &pid);
 
 		std::string current_dir = get_current_directory();
 
-		// Load the injector api
 		HMODULE inject_module = LoadLibraryA(std::string(current_dir + "hyperion_inject.dll").c_str());
 		if (!inject_module)
 			return 0;
 
-		std::cout << "Injecting... Module: " << std::hex << inject_module << std::endl;
+		const DWORD target_thread_id = get_target_thread_id(pid, L"RakNet::RecvFromLoop");
+		std::cout << "Injecting on thread " << target_thread_id << "." << std::endl;
 
-		// Locate the two api functionz
 		t_inject f_inject = reinterpret_cast<t_inject>(GetProcAddress(inject_module, "inject"));
 		t_update f_update = reinterpret_cast<t_update>(GetProcAddress(inject_module, "update"));
 
-		// Inject the dll into the process id 
-		if (f_inject(pid, std::string(current_dir + "hello_world.dll").c_str()) != 0)
+		if (f_inject(pid, target_thread_id, std::string(current_dir + "hello_world.dll").c_str()) != 0)
 		{
 			while (f_update());
 
